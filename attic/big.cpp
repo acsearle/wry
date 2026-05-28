4bcd17ee01bcb87fe2bed850deb5eb94294451a3
Antony Searle <antony.searle@gmail.com>
2026-05-28 18:26:12 +1000

    burn down attic

--- attic/big.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  big.cpp
-//  client
-//
-//  Created by Antony Searle on 17/10/2023.
-//
-
-#include "big.hpp"
--- attic/channel.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  channel.cpp
-//  client
-//
-//  Created by Antony Searle on 17/7/2025.
-//
-
-#include "channel.hpp"
